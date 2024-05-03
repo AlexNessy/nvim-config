31f5192a2b7f7747ed56b5c1e87fe4b143603a71
AlexNessy <alexnssy@gmail.com>
2024-05-03 23:20:09 +0100

    New Changes

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,9 +4,9 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.selene,
 				null_ls.builtins.formatting.shellharden,
-        null_ls.builtins.formatting.autoflake,
+        null_ls.builtins.formatting.shfmt,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,47 +1,53 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		config = function()
-			local wk = require("which-key")
-			wk.register({
-				["<C-p>"] = { "Find File" },
-				["<C-n>"] = { ":Neotree left <CR>", "NeoTree" },
-				["<C-l>"] = { "<C-w>w", "Switch Windows" },
-				["<C-e>"] = { "Show Harpoon List" },
-				K = { "Define Function" },
-				["<leader>"] = {
-					z = { ":wqa <CR>", "Quit All" },
-					q = { ":q! <CR>", "Quit buffer" },
-					w = { ":w <CR>", "Save buffer" },
-					a = { "Add to Harpoon List" },
-					r = { "Remove from Harpoon List" },
-					f = {
-						name = "File",
-						g = { "Live Grep" },
-					},
-					g = {
-						name = "Formatting",
-						f = { "Format" },
-					},
-					u = { "Incline" },
-					c = {
-						name = "Code",
-						a = { "Action" },
-					},
-					e = {
-						name = "NeoTree Options",
-						e = {
-							":Neotree toggle left<CR>",
-							"Toggle Neotree",
-						},
-					},
-				},
-			})
-		end,
-	},
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    config = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<C-p>"] = { "Find File" },
+        ["<C-n>"] = { ":Neotree left <CR>", "NeoTree" },
+        ["<C-l>"] = { "<C-w>w", "Switch Windows" },
+        ["<Tab>"] = { ":bnext<CR>", "Next Buffer" },
+        ["<S-Tab>"] = { ":bprev<CR>", "Previous Buffer" },
+        K = { "Define Function" },
+        ["<leader>"] = {
+          z = { ":wqa <CR>", "Quit All" },
+          q = { ":q! <CR>", "Quit buffer" },
+          w = { ":w <CR>", "Save buffer" },
+          f = {
+            name = "File",
+            g = { "Live Grep" },
+          },
+          g = {
+            name = "Formatting",
+            f = { "Format" },
+          },
+          u = { "Incline" },
+          c = {
+            name = "Code",
+            a = { "Action" },
+          },
+          e = {
+            name = "NeoTree Options",
+            e = {
+              ":Neotree toggle left<CR>",
+              "Toggle Neotree",
+            },
+          },
+          b = {
+            name = "Buffer",
+            d = {
+              ":bdelete<CR>",
+              "Delete Buffer",
+            },
+          },
+        },
+      })
+    end,
+  },
 }
