8b55648423c6addb701a668236d5fe749f69bf77
AlexNessy <alexnessy@icloud.com>
2024-05-22 22:10:17 +0100

    New Changes

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,6 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.selene,
 				null_ls.builtins.formatting.shellharden,
         null_ls.builtins.formatting.shfmt,
 			},
